08dc675323abf329d17c3d0b6b7af2b606103008
KevRoBot <muffinthc@gmail.com>
2022-07-06 23:50:59 -0500

    v0.4

---
+++ js/prueba.js
@@ -0,0 +1,44 @@
+/*var meses=["enero,febrero ,marzo,abril ,mayo ,junio,julio,agosto,septiembre,octubre,noviembre,diciembre"];
+alert("LOS MESES SON: "+meses);
+
+
+var valores=[true,5,false,"Hola","adios",2];
+var textomayor=valores[3]>valores[4];
+alert("El texto mayor es:"+textomayor);
+
+var v1=valores[0];
+var v2=valores[2];
+
+var res=v1 || v2;
+alert("Booleanos: "+res);
+ 
+res=v1 && v2;
+alert("Booleanos: "+res);
+
+var num1=valores[1];
+var num2=valores[5];
+
+var suma=num1+num2;
+alert("Suma: "+suma);
+
+var resta=num1-num2;
+alert("Resta: "+resta);
+
+var multi=num1*num2;
+alert("Multiplicacion: "+multi);
+
+var divi= num1/num2;
+alert("Division: "+divi);
+
+var mod=num1%num2;
+alert("Modulo: "+mod);
+*/
+
+const num=10;
+var r=1;
+for(var i=num;i>0;i++){
+    r*i;
+
+}
+alert("El factorial es : "+r);
+
